21174147bb350c9d6a2728354c7e03da9a62eaa6
Russell Cheung <russell.cheung@finddoc.com>
2012-04-19 12:27:20 +0800

    fix some unimplemented protocol warnings (method body still unimplemented) remove some conflicting flickr files (use submodule files) update facebook sdk

--- Classes/ShareKit/Sharers/Services/Facebook/SHKFacebook.m
+++ Classes/ShareKit/Sharers/Services/Facebook/SHKFacebook.m
@@ -350,6 +350,19 @@ static NSString *const kSHKFacebookUserInfo =@"kSHKFacebookUserInfo";
     [self release]; //see [self promptAuthorization]
 }
 
+- (void)fbDidExtendToken:(NSString*)accessToken
+               expiresAt:(NSDate*)expiresAt {
+    
+}
+
+- (void)fbDidLogout {
+    
+}
+
+- (void)fbSessionInvalidated {
+    
+}
+
 #pragma mark FBRequestDelegate methods
 
 - (void)requestLoading:(FBRequest *)request
